b4929f92357cdee7b8bd22eea8cb0d0dfd3aad41
dxxxxy <introsdream@gmail.com>
2023-08-13 23:29:34 -0400

    feat: unsubscribe from webhook

---
+++ src/routes/v1/unsubscribe.ts
@@ -0,0 +1,20 @@
+import { Response } from "express"
+import Webhook from "../../model/Webhook.js"
+
+export default async(req: any, res: Response) => {
+    //get webhook id
+    const id = req.params.id
+    if (!id) return res.status(400).send("No id provided")
+
+    //delete webhook from database
+    Webhook.findByIdAndDelete(id, (err: any, webhook: any) => {
+        if (err) {
+            console.error(err)
+            return res.status(500)
+        }
+
+
+        //send back 200
+        res.status(200)
+    })
+}
