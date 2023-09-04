39ae48a2e6c15ae809cebb6678df296b93a4f3ed
dxxxxy <introsdream@gmail.com>
2023-09-03 21:04:52 -0400

    feats: use different rpc method add fixes add error handling

--- src/routes/local/notify.ts
+++ src/routes/local/notify.ts
@@ -1,47 +1,43 @@
 import crypto from "crypto"
-import { Response } from "express"
+import { Request, Response } from "express"
 import { client } from "../../app.js"
 import Webhook, { IWebhook } from "../../model/Webhook.js"
 
-/* this request comes from bitcoin-core's walletnotify, therefore there is no validation needed */
-export default (req: any, res: Response) => {
-    //only accept local connections
-    if (req.connection.localAddress !== req.connection.remoteAddress) res.status(403)
-
+export default (req: Request, res: Response) => {
     //get txid from walletnotify (guaranteed to be there)
     const txid = req.params.txid
 
     //get transaction from txid
-    client.getrawtransaction({ txid, verbose: true }).then(tx => {
+    client.gettransaction({ txid, include_watchonly: true, verbose: true }).then(async tx => {
         //get all output addresses
-        const addresses = tx.vout.map((vout: { scriptPubKey: { addresses: String[] } }) => vout.scriptPubKey.addresses).flat()
+        const addresses = tx.decoded.vout.map((vout: { scriptPubKey: { address: String } }) => vout.scriptPubKey.address)
 
-        //get webhooks from database
-        Webhook.find({ Address: { $in: addresses }}, (err: any, webhooks: IWebhook[]) => {
-            if (err) {
-                console.error(err)
-                return res.status(500)
-            }
+        //get webhooks from database that match at least one address
+        const webhooks: IWebhook[] = await Webhook.find({ Address: { $in: addresses }})
 
-            //send webhooks
-            webhooks.forEach(webhook => {
-                const body = JSON.stringify({ tx, address: webhook.Address })
+        //send webhooks
+        webhooks.forEach(webhook => {
+            //create body
+            const body = JSON.stringify({ tx, address: webhook.Address })
 
-                const signature = crypto.createHmac("sha256", webhook.Secret).update(body).digest("hex")
+            //create signature
+            const signature = crypto.createHmac("sha256", webhook.Secret).update(body).digest("hex")
 
-                //noinspection JSIgnoredPromiseFromCall
-                fetch(webhook.Callback, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "X-Webhook-Signature": signature
-                    },
-                    body
-                })
+            //send
+            fetch(webhook.Callback, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Webhook-Signature": signature
+                },
+                body
             })
-
-            //end curl task in walletnotify
-            res.status(200)
         })
+
+        //end curl task in walletnotify
+        res.status(200)
+    }).catch(err => {
+        console.error(err)
+        res.status(500)
     })
 }
